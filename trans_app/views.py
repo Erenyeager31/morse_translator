6e7e1eef70d94f0fdeb178e0de5dbeb83035cb40
Dishant Shah <dishantshah3133@gmail.com>
2024-08-23 22:21:23 +0530

    Changes for Portfolio

--- trans_app/views.py
+++ trans_app/views.py
@@ -52,20 +52,22 @@ def mtot(request):
     value = ""
     if request.method == "POST":
         input = request.POST.get('texti')
-        input.replace("_","-")
-        input_array = input.split(" ")
-        for i in input_array:
-            print(morse_to_text[i])
-            value += morse_to_text[i]
-        
-    conversion = {
-        "value":value
-    }
-    # input_array = input.split(" ")
-    
+        input = input.replace("_","-")
+        input_array = input.split("  ")  # Split by double spaces
 
-    # print(input_array)
-    return render(request,'morse_to_text.html',conversion)
+        for i in input_array:
+            if i.strip():  # Ensure we don't process empty strings
+                words = i.split(" ")  # Split by single space to get individual Morse codes
+                for word in words:
+                    if word in morse_to_text:
+                        value += morse_to_text[word]
+                value += " "  # Add a single space between the converted words
+
+    conversion = {
+        "value": value.strip()  # Strip any leading or trailing spaces
+    }
+    
+    return render(request, 'morse_to_text.html', conversion)
 
 def ttom(request):
     text_to_morse = {
